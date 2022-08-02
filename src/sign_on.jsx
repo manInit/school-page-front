ff8c6c8b8bdd7b15346363e2e208f4774b9e93eb
anvy <0073av@gmail.com>
2022-08-02 10:33:44 +0800

    new abstraction, post req connected
    fix validation

--- src/sign_on.jsx
+++ src/sign_on.jsx
@@ -2,32 +2,33 @@ import { useEffect, useState } from "react";
 import { home_btn } from "./home";
 import { post_req } from "./utils/requests-api";
 
+
+const values = {
+    name: '',
+    surname: '',
+    fatherName: '',
+    educationalOrganization: '',
+    educationalClass: '',
+    email: '',
+    phoneNumber: ''
+}
+const falses = (()=>{
+    const a = []
+    for (let i in values)
+        a[i] = !!values[i]
+    return a
+})()
+
 const Sign_on = () =>{
     useEffect(() => {
         document.title = `Регистрация`;
     })
     // const values = Array(7).fill('', 0,7)
-
-    const values = {
-        name: '',
-        surname: '',
-        patronym: '',
-        school: '',
-        class: '',
-        email: '',
-        phone: ''
-    }
-    const falses = (()=>{
-        const a = []
-        for (let i in values)
-            a[i] = !!values[i]
-        return a
-    })()
     const [invalids, setInvalids] = useState(falses)    
     // console.log(invalids)      
     const subtext = <span style={{color:'red', fontSize: '80%'}}>*недопустимый формат</span>
     const input_type = (type, name) =>{
-        return <input type={type} name={name} onChange={e=>values[name]=e.target.value}/>
+        return <input type={type} name={name} onChange={e=>{values[name]=e.target.value}}/>
     }
     
     const register = (data) =>{
@@ -52,9 +53,9 @@ const Sign_on = () =>{
             switch(i){
                 case 'name':
                 case 'surname':
-                case 'patronym':
+                case 'fatherName':
                     if (!new RegExp('^[a-zA-Zа-яА-Я]+$').test(data[i])) inds.push(i)
-                    break
+                break
                 default: break
             }
         }
@@ -62,7 +63,14 @@ const Sign_on = () =>{
         // else if (!new RegExp('^[0-9]+$').test(data[6])) inds.push(6)
         return inds
       }
-
+    const line_field = (lable, field, type) =>{
+        return(
+            <>
+                <tr><td>{lable} </td><td>{input_type(type, field)}</td></tr>
+                <tr><td></td><td>{invalids[field]&&subtext}</td></tr>
+            </>
+        )
+    }
 
     return(
         <>
@@ -70,21 +78,13 @@ const Sign_on = () =>{
             <table>
                 <caption style={{backgroundColor: 'orange'}}>Регистрация:</caption>
                 <tbody>
-                    <tr><td>Имя: </td><td>{input_type('text', 'name')}</td></tr>
-                    <tr><td></td><td>{invalids['name']&&subtext}</td></tr>
-                    <tr><td>Фамилия: </td><td>{input_type('text', 'surname')}</td></tr>
-                    <tr><td></td><td>{invalids['surname']&&subtext}</td></tr>
-                    <tr><td>Отчество: </td><td>{input_type('text', 'patronym')}</td></tr>
-                    <tr><td></td><td>{invalids['patronym']&&subtext}</td></tr>
-                    <tr><td>Школа: </td><td>{input_type('text', 'school')}</td></tr>
-                    <tr><td></td><td>{invalids['school']&&subtext}</td></tr>
-                    <tr><td>Класс: </td><td>{input_type('text', 'class')}</td></tr>
-                    <tr><td></td><td>{invalids['class']&&subtext}</td></tr>
-                    <tr><td>Почта: </td><td>{input_type('text', 'email')}</td></tr>
-                    <tr><td></td><td>{invalids['email']&&subtext}</td></tr>
-                    <tr><td>Телефон: </td><td>{input_type('tel', 'phone')}</td></tr>
-                    <tr><td></td><td>{invalids['phone']&&subtext}</td></tr>
-
+                    {line_field('Имя:','name', 'text')}
+                    {line_field('Фамилия:','surname', 'text')}
+                    {line_field('Отчество:','fatherName', 'text')}
+                    {line_field('Школа:','educationalOrganization', 'text')}
+                    {line_field('Класс:','educationalClass', 'text')}
+                    {line_field('Почта:','email', 'email')}
+                    {line_field('Телефон:','phone', 'tel')}
                     <tr><td align="center" colSpan={2}><input type='button' value='Регистрация' style={{display: 'inline-block', width:'100%'}} onClick={()=>register(values)}/></td></tr>
                 </tbody>
             </table>
