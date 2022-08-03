2498ccb10b33f7244190546c3a76086281384e13
anvy <0073av@gmail.com>
2022-08-04 00:06:45 +0800

    popup pattern

---
+++ src/popup/popup_modal.jsx
@@ -0,0 +1,13 @@
+import './popup_modal.css'
+
+const Popup_modal = ({active, setActive, children}) =>{
+    return(
+        <div className={active ? "popup_block active" : "popup_block"} onClick={() => setActive(false)}>
+            <div className="popup_content" onClick={e => e.stopPropagation()}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Popup_modal
