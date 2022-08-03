de0b2e046acc358149a178ab9f0674bb656911d2
anvy <0073av@gmail.com>
2022-08-04 00:08:28 +0800

    trajectory page

--- src/trajectory.jsx
+++ src/trajectory.jsx
@@ -1,7 +1,47 @@
+import { useEffect } from "react";
+import { home_btn, personal_space_btn, bottom_info, uni_btn, user_btn, login_btn, register_btn, base_url, trajectory_btn } from "./home"
 
 
-const TrajectoryInfo = () =>{
-
+const Trajectory = () =>{
+    useEffect(() => {
+        document.title = `Направление`;
+    })
+    const trajectories = [{},{},{},{},{},{},{},{},{},{}]//get_req('')
+    return(
+        <>
+            <div className="head_wrap">
+                <div className="head_panel">
+                    {uni_btn}
+                    {home_btn}
+                    {trajectory_btn}
+                    {login_btn}
+                    {register_btn}
+                    {/* {user_btn} */}
+                    {personal_space_btn}
+                </div>
+            </div>
+            <span className="title_span">Trajectory name</span>
+            <div className="personal_trajectories">
+                <div /*key={elem.id}*/ className="personal_trajectories_item" onClick={()=>{}}>
+                    Траектория развития
+                </div>
+                {trajectories.map(elem =>(
+                    trajectory_div(elem)    
+                ))}
+            </div>
+            {bottom_info}
+        </>
+    )
 }
 
-export default TrajectoryInfo
+export default Trajectory
+
+const trajectory_div = (elem) =>{
+    return(
+        <>
+            <div /*key={elem.id}*/ className="trajectory_activity" onClick={()=>{window.location.href=base_url+'/activity'}}>
+                Активность
+            </div>
+        </>
+    )
+}
