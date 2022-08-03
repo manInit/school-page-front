2607bb13d8d9977cd682731413178aae569707eb
anvy <0073av@gmail.com>
2022-08-03 20:15:11 +0800

    past activities front

--- src/personal_components/past_activity_list.jsx
+++ src/personal_components/past_activity_list.jsx
@@ -1,11 +1,29 @@
 
 
 const PastActivityList = () =>{
+    const list = [{},{},{},{}]
     return(
         <>
-            PastActivityList
+            {list.map(elem =>(
+                elem_div(elem)
+            ))}
         </>
     )
 }
 
 export default PastActivityList
+
+const elem_div = (elem) =>{
+    return(
+        <div className="" style={{display: "flow-root", marginBottom: '8px', border: '1px solid black'}} onClick={()=>{}}>
+            <div style={{float: "left"}}>
+                <span >Описание Мероприятия</span><br></br>
+                <span >Дата и место</span><br></br><br></br>
+                <span >Название Мероприятия</span>
+            </div>
+            <div style={{float: "right"}}>
+                <span style={{float: 'right'}}>молодец</span>
+            </div>
+        </div>
+    )
+}
