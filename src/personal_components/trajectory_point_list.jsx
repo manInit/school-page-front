e0dc9d6497490acf9191fc839e0deeeeb46c7892
anvy <0073av@gmail.com>
2022-08-03 20:14:57 +0800

    trajectory score_list front

--- src/personal_components/trajectory_point_list.jsx
+++ src/personal_components/trajectory_point_list.jsx
@@ -1,12 +1,24 @@
 
 
 const TrajectoryPointList = () =>{
+    const list = [{},{},{},{}]
     return(
         <>
-            TrajectoryPointList
+            {list.map(elem =>(
+                elem_div(elem)
+            ))}
         </>
     )
     
 }
 
 export default TrajectoryPointList
+
+const elem_div = (elem) =>{
+    return(
+        <div className="">
+            <span >Направление</span>
+            <span style={{float: 'right'}}>n балла(ов)</span>
+        </div>
+    )
+}
