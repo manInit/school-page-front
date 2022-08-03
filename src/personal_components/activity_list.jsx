a426c76a87553dae559b44ce58ce182eaad42a7a
anvy <0073av@gmail.com>
2022-08-03 20:15:25 +0800

    present activities front

--- src/personal_components/activity_list.jsx
+++ src/personal_components/activity_list.jsx
@@ -1,11 +1,26 @@
 
 
 const ActivityList = () =>{
+    const list = [{},{},{},{}]
     return(
         <>
-            ActivityList
+            {list.map(elem =>(
+                elem_div(elem)
+            ))}
         </>
     )
 }
 
 export default ActivityList
+
+const elem_div = (elem) =>{
+    return(
+        <div className="" style={{display: "flow-root", marginBottom: '8px', border: '1px solid black'}} onClick={()=>{}}>
+            <div style={{float: "left"}}>
+                <span >Описание Мероприятия</span><br></br>
+                <span >Дата и место</span><br></br><br></br>
+                <span >Название Мероприятия</span>
+            </div>
+        </div>
+    )
+}
