826b669ff0be10774a6fcdffa21aeca1f817d49e
anvy <0073av@gmail.com>
2022-08-08 15:22:57 +0800

    refactor profile

--- src/personal_components/profile.jsx
+++ src/personal_components/profile.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Popup_modal from "../popup/popup_modal";
+
 const values ={
     name: 'Вова',
     surname: 'Гельштейн',
@@ -9,46 +10,31 @@ const values ={
     email: 'чёта',
     phoneNumber: '+7832923'
 }
-// const values ={
-//     name: 'Вова',
-//     surname: 'Гельштейн',
-//     fatherName: 'Максимович',
-//     educationalOrganization: 'мяумяуСОШ',
-//     educationalClass: '10В',
-//     email: 'чёта',
-//     phoneNumber: '+7832923'
-// }
-
-
 
 const subtext = <span style={{color:'red', fontSize: '80%'}}>*недопустимый формат</span>
-const input_type = (type, name) =>{
-    return <input type={type} name={name} onChange={e=>{values[name]=e.target.value}}/>
-}
-
 
 const Profile = () =>{
-    const [active_span, set_active_span] = useState()    
+    const [chosen_span, set_chosen_span] = useState()
+    
     let new_value
-    const [span_text, set_span_text] = useState(values)    
-    console.log(span_text)
+    let put_link
+    const [data, set_data] = useState(values)    
+    console.log(data)
     const [popup_visible, set_popup_visible] = useState(false)    
-    const [invalids, setInvalids] = useState(false)
-    const input_new_value = <input type='text' defaultValue = {values[active_span]} onChange={(e)=>{if(e.target.value!==undefined) new_value = e.target.value}} />
-    const line_field = (lable, field) =>{
+    const [invalid, setInvalid] = useState(false)
+    const edit_data = <input type='text' defaultValue = {values[chosen_span]} onChange={(e)=>{if(e.target.value!==undefined) new_value = e.target.value}} />
+    const view_data = (lable, field, type, link) =>{
         return(
             <>
-                <tr><td>{lable} </td><td>{span_text[field]}</td><td>{edit_btn(field, '/api/athlet/name')}</td></tr>
+                <tr><td>{lable} </td><td>{data[field]}</td><td>{edit_btn(field, type, link)}</td></tr>
             </>
         )
     }
-    const change_value = (target, link) =>{
+    const change_value = (link) =>{
 
-        
-
-        console.log(`${active_span} `)
+        console.log(`${chosen_span} `)
         console.log(values)
-        switch(active_span){
+        switch(chosen_span){
             case 'name':
             case 'surname':
             case 'fatherName':
@@ -58,40 +44,42 @@ const Profile = () =>{
         }
     }
     const show_invalid = (cond) =>{
-        console.log(cond, invalids, new_value)
-        if(cond === invalids){
-            setInvalids(!cond)
+        console.log(cond, invalid, new_value)
+        if(cond === invalid){
+            setInvalid(!cond)
         }
         if (cond){
             set_popup_visible(false)
-            span_text[active_span] = new_value
-            set_span_text(arr => ({...arr}))
+            data[chosen_span] = new_value
+            set_data(arr => ({...arr}))
+            //put_req(put_link)
         }
 
     }
-    const edit_btn = (field, link) =>{
-        return <input type="button" value="✐" onClick={() => { set_popup_visible(true); set_active_span(field)}}></input>
+    const edit_btn = (field, type, link) =>{
+        return <input type="button" value="✐" onClick={() => { set_popup_visible(true); set_chosen_span(field); put_link=link}}></input>
     }
     return(
         <>
             <Popup_modal active={popup_visible} setActive={set_popup_visible}>
-                {popup_visible&&input_new_value}
+                {popup_visible&&edit_data}
                 <br></br>
-                {invalids&&subtext}
-                <br></br><br></br>
+                {invalid&&subtext}
+                <br></br>
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                     <input type='button' value='изменить' onClick={()=>change_value()}/>
                 </div>
             </Popup_modal>
             <table>
             <tbody>
-                {line_field('Имя', 'name', 'text')}
-                {line_field('Фамилия:','surname', 'text')}
-                {line_field('Отчество:','fatherName', 'text')}
-                {line_field('Школа:','educationalOrganization', 'text')}
-                {line_field('Класс:','educationalClass', 'text')}
-                {line_field('Почта:','email', 'email')}
-                {line_field('Телефон:','phoneNumber', 'tel')}
+                {/* <view_data lable={'Имя'} field={'name'} link={'/api/athlet/name'}/> */}
+                {view_data('Имя', 'name', 'text', '/api/athlet/name')}
+                {view_data('Фамилия:','surname', 'text')}
+                {view_data('Отчество:','fatherName', 'text')}
+                {view_data('Школа:','educationalOrganization', 'text')}
+                {view_data('Класс:','educationalClass', 'text')}
+                {view_data('Почта:','email', 'email')}
+                {view_data('Телефон:','phoneNumber', 'tel')}
             </tbody>
         </table>
         </>
