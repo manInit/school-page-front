306aff1df83de8441e3c872951ce84b39fe5afee
manInit <info.denisbronnikov@gmail.com>
2022-09-10 19:40:12 +0800

    Test deploy commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
+      Test Deploy
       <Router>
         <>
           <Routes>
