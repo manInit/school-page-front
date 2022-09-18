084d240c73cd534de820541f4f6873e78c343337
anvy <0073av@gmail.com>
2022-09-18 20:20:54 +0800

    registration route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/main-page/MainPage';
+import RegistrationPage from './pages/main-page/RegistrationPage';
 
 import './scss/index.scss';
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
         </Routes>
       </BrowserRouter>
     </div>
