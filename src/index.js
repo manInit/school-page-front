63b77b7dfd11d8e48024bf4fb04ea4e829a649e8
anvy <0073av@gmail.com>
2022-10-09 14:22:38 +0800

    new lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+library.add(faEye);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
