d72e7bf61b5843d26427271a420a99a3882000f8
manInit <info.denisbronnikov@gmail.com>
2022-10-31 21:56:16 +0800

    add admin test enter

--- src/private-route.jsx
+++ src/private-route.jsx
@@ -1,11 +1,11 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import authStore from './store/auth';
 
-const PrivateRoute = () => {
+const PrivateRoute = observer(() => {
   const auth = authStore.isAuth;
-  return auth ? <Outlet /> : <Navigate to="/login" />;
-};
+  return auth ? <Outlet /> : <Navigate to='/login' />;
+});
 
 export default PrivateRoute;
-
