7e43e73d3075cefdc5d1fdab3d30a3590eaebb4c
anvy <0073av@gmail.com>
2022-11-15 12:32:27 +0800

    new icon

--- src/utils/fontawesome-icons.js
+++ src/utils/fontawesome-icons.js
@@ -1,3 +1,3 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEye, faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
-library.add(faEye, faXmark, faCheck);
+import { faEye, faXmark, faCheck, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+library.add(faEye, faXmark, faCheck, faMagnifyingGlass);
